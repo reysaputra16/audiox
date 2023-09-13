6a5b2ef6199a7f88c949c14bf131ce9ef5b2e3f8
reynardsaputra <ge35qum@mytum.de>
2023-09-13 14:55:50 +0700

    added database initializations (edit)

--- web_app/app.py
+++ web_app/app.py
@@ -270,7 +270,7 @@ def remove_word():
     return redirect("/about")
 
 if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
     db.drop_all()
     db.create_all()
-    app.run(host='0.0.0.0', port=5000)
     
